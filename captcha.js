b515c4562759cbb7f9a2e2e37870f291f8092303
amin <121653089+itsaminowo@users.noreply.github.com>
2023-01-26 15:43:57 -0500

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -1,8 +1,3 @@
- function autoRefresh() {
-        window.location = window.location.href;
-    }
-    setInterval('autoRefresh()', 5000);
-}
 function generateCaptcha()
          {
              var alpha = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','&','-','=','+');
