9f2d2331f4838aabd3a9c687245c910b6c41446c
amin <121653089+itsaminowo@users.noreply.github.com>
2023-01-30 09:48:47 -0500

    Update script.js

--- external/script.js
+++ external/script.js
@@ -26,9 +26,6 @@ function search_page() {
         }
     }
 }
-    document.getElementById("errorRedirect").onclick = function () {
-        location.href = "https://itsaminowo.github.io/findoutsearch/";
-    };
 window.onload = function() {
       var month = new Array("January", "February", "March", "April", "May", "June", "July",    "August", "September", "October", "November", "December");
       var today = new Date();
